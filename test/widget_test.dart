3090a7e8ebaa67f148b34a1631350e3f443e2807
Simon Meyer <simonmeyerrr@gmail.com>
2020-03-23 18:48:04 +0100

    login page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:solutionchallenge/main.dart';
+import 'package:solution_challenge/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
