8f280698d5aa7773cb4b84b0b2451832a78d7321
Paul Decrosse <paul.decrosse@epitech.eu>
2020-03-25 20:22:20 +0100

    handle tap but it's not crazy

--- lib/popo/draw_details.dart
+++ lib/popo/draw_details.dart
@@ -22,22 +22,22 @@ class DrawDetails extends StatelessWidget {
             imgPath,
           ),
           Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: Container(
-                    height: 60,
-                    width: 60,
-                    child: FloatingActionButton(
-                        heroTag: "btnAR",
-                        materialTapTargetSize: MaterialTapTargetSize.padded,
-                        child: const Icon(Icons.photo_camera, size: 36.0),
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => DrawAR(imgPath: imgPath,)));
-                      },
-                    )
-                ),
-              )
+            padding: const EdgeInsets.all(16.0),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                height: 60,
+                width: 60,
+                child: FloatingActionButton(
+                  heroTag: "btnAR",
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  child: const Icon(Icons.photo_camera, size: 36.0),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => DrawAR(imgPath: imgPath,)));
+                  },
+                )
+              ),
+            )
           ),
         ]
       ),
@@ -66,31 +66,74 @@ class _DrawARState extends State<DrawAR> {
         title: Text("Drawing in real life"),
         backgroundColor: Theme.of(context).accentColor,
       ),
-      body: ArCoreView(
-        onArCoreViewCreated: _onArCoreViewCreated,
+      body: Stack(
+        children: <Widget>[
+          ArCoreView(
+            enableTapRecognizer: true,
+            onArCoreViewCreated: _onArCoreViewCreated,
+          ),
+          Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                    height: 60,
+                    width: 60,
+                    child: FloatingActionButton(
+                      heroTag: "btnARCenter",
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+                      child: const Icon(Icons.center_focus_strong, size: 36.0),
+                      onPressed: () {
+                        arCoreController.removeNode(nodeName: "draw");
+                        _addCube(null);
+                      },
+                    )
+                ),
+              )
+          ),
+        ],
       ),
     );
   }
 
   void _onArCoreViewCreated(ArCoreController controller) {
     arCoreController = controller;
-
-    _addCube(arCoreController);
+    arCoreController.onPlaneTap = _handleOnPlaneTap;
+    _addCube(null);
   }
 
-  Future<void> _addCube(ArCoreController controller) async {
-    final material = ArCoreMaterial(
+  void _handleOnPlaneTap(List<ArCoreHitTestResult> hits) {
+    final hit = hits.first;
+    arCoreController.removeNode(nodeName: "draw");
+    _addCube(hit);
+  }
+
+  Future<void> _addCube(ArCoreHitTestResult plane) async {
+    var material = ArCoreMaterial(
         color: Colors.white,
         textureBytes: await networkImageToByte(imgPath)
     );
-    final cube = ArCoreCube(
+    var cube = ArCoreCube(
       materials: [material],
       size: vector.Vector3(0.5, 1, 0),
     );
-    final node = ArCoreNode(
-        shape: cube
+    var node = ArCoreNode(
+      name: "draw",
+      shape: cube,
+      position: vector.Vector3(0, 0, -1.5),
+      rotation: vector.Vector4(0, 0, 0, 0)
     );
-    controller.addArCoreNode(node);
+    if (plane != null) {
+      plane.pose.rotation.z = -plane.pose.rotation.z;
+      plane.pose.translation.y += 1;
+      node = ArCoreNode(
+          name: "draw",
+          shape: cube,
+          position: plane.pose.translation,
+          rotation: plane.pose.rotation
+      );
+    }
+    arCoreController.addArCoreNode(node);
   }
 
   @override
